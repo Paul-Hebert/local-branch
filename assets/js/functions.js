64f2fc6ad8f21a10527bb9d2e1f390f61a5c58d0
QDarius <paul.hebert@paulhebertdesigns.com>
2016-03-07 13:01:39 -0800

    Hamburger icon animates

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,6 +8,49 @@ $(function(){
 			$('#menu-toggle svg').attr('class','open');
 		}
 
+		var first_y = $('.first-line').attr('y2');
+		var last_y = $('.last-line').attr('y2');
+
+		//$('.first-line').attr('y2', last_y);
+		//$('.last-line').attr('y2', first_y);
+
+		animate(
+	        $('.first-line'), // target jQuery element
+	        { y2: last_y}, // target attributes
+	        300 // optional duration in ms, defaults to 400
+	    );
+		animate(
+	        $('.last-line'), // target jQuery element
+	        { y2: first_y}, // target attributes
+	        300 // optional duration in ms, defaults to 400
+	    );
+
+
 		$('header nav').toggleClass('visible-y');
 	});
 });
+
+  function animate($el, attrs, speed) {
+        speed = speed || 400;
+        var start = {},
+            timeout = 20,
+            steps = Math.floor(speed/timeout),
+            cycles = steps;
+        
+        $.each(attrs, function(k,v) {
+            start[k] = $el.attr(k);
+        });
+        
+        (function loop() {
+            $.each(attrs, function(k,v) {
+                var pst = (v - start[k])/steps;
+                $el.attr(k, function(i, old) {
+                    return +old + pst;
+                });
+            });
+          if ( --cycles )
+              setTimeout(loop, timeout);
+          else
+              $el.attr(attrs);
+        })();
+    }
